cfc012dbbaf6575332227446d98d6eeb7421cb2f
Vladislav Georgiev <vlad.georgiev@outlook.com>
2019-02-25 18:40:52 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,13 +1,25 @@
 function basicTeenager(age) {
-
+  if(age > 13 && age < 19) {
+    "You are a teenager!"
+  }
 }
 
 function teenager(age) {
-
+  if(age > 13 && age < 19) {
+  "You are a teenager!"
+  } else {
+  "You are not a teenager!"
+  }
 }
 
 function ageChecker(age) {
-
+  if(age > 13 && age < 19) {
+  "You are a teenager!"
+  } else if(age < 12) {
+  "You are a kid"
+  } else(age > 19) {
+  "You are a grownup!"
+  }
 }
 
 function ternaryTeenager(age) {
